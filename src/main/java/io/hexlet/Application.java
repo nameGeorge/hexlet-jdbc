832dcf8577e3e08707d03340d1fe914908e988e1
george <geomel@yandex.ru>
2024-11-14 22:02:55 +0300

    placeholders

--- src/main/java/io/hexlet/Application.java
+++ src/main/java/io/hexlet/Application.java
@@ -2,6 +2,7 @@ package io.hexlet;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Application {
     // Нужно указывать базовое исключение,
@@ -15,17 +16,64 @@ public class Application {
             try (var statement = connection.createStatement()) {
                 statement.execute(query);
             }
-            var query2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-            try (var statement2 = connection.createStatement()) {
-                statement2.executeUpdate(query2);
+            var query2 = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (var preparedStatement = connection.prepareStatement(query2)) {
+                preparedStatement.setString(1, "tommy");
+                preparedStatement.setString(2, "123456789");
+                preparedStatement.executeUpdate();
+                preparedStatement.setString(1, "timmy");
+                preparedStatement.setString(2, "1222222");
+                preparedStatement.executeUpdate();
+                preparedStatement.setString(1, "tammy");
+                preparedStatement.setString(2, "123333333");
+                preparedStatement.executeUpdate();
             }
             var query3 = "SELECT * FROM users";
-            try (var statement3 = connection.createStatement()) {
-                var resultSet = statement3.executeQuery(query3);
+            try (var statement = connection.createStatement()) {
+                var resultSet = statement.executeQuery(query3);
                 while (resultSet.next()) {
                     System.out.println(resultSet.getLong("id"));
                     System.out.println(resultSet.getString("username"));
                     System.out.println(resultSet.getString("phone"));
+                }
+            }
+            System.out.println("\n\n");
+            var queryDeleteUser = "DELETE FROM users WHERE phone = ?";
+            try (var preparedStatement = connection.prepareStatement(queryDeleteUser)) {
+                preparedStatement.setString(1,"123333333");
+                preparedStatement.executeUpdate();
+            }
+            var query4 = "SELECT * FROM users";
+            try (var statement = connection.createStatement()) {
+                var resultSet = statement.executeQuery(query4);
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getLong("id"));
+                    System.out.println(resultSet.getString("username"));
+                    System.out.println(resultSet.getString("phone"));
+                }
+            }
+            //Возврат id нового объекта внесенного в базу
+            var sql = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (var preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, "Sarah");
+                preparedStatement.setString(2, "333333333");
+                preparedStatement.executeUpdate();
+                var generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    System.out.println(generatedKeys.getLong(1));
+                } else {
+                    throw new SQLException("DB have not returned an id after saving the entity");
+                }
+                preparedStatement.setString(1, "Peter");
+                preparedStatement.setString(2, "3234234523");
+                preparedStatement.executeUpdate();
+                // Если ключ составной, значений может быть несколько
+                // В нашем случае, ключ всего один
+                generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    System.out.println(generatedKeys.getLong(1));
+                } else {
+                    throw new SQLException("DB have not returned an id after saving the entity");
                 }
             }
         }
